8b0001a80406847e2da280ab56657f849d627cb8
Elise St Hilaire and Shruti Priya <elise+shruti@>
2017-02-09 10:23:44 -0800

    add object and new views not compiling properly

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using System.Collections.Generic;
 using CDProject.Objects;
 
 namespace CDProject
@@ -8,6 +9,12 @@ namespace CDProject
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+      Get["/cd_form"] = _ => View["Add_Cd.cshtml"];
+      Post["/view_Cd"] = _ => {
+        Album newAlbum = new Album(Request.Form["add-cd"], Request.Form["add-artist"]);
+        newAlbum.Save();
+        return View["view_Cd.cshtml", newAlbum];
+      };
     }
   }
 }
---
+++ Objects/cd.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace CDProject.Objects
+{
+  public class Album
+  {
+    private string _title;
+    private string  _artist;
+    private int _id;
+    private static List<Album> _albumList = new List<Album>{};
+
+    public Album (string title, string artist)
+    {
+      _title = title;
+      _artist = artist;
+    }
+    public string GetTitle()
+    {
+      return _title;
+    }
+
+    public void SetTitle(string newTitle)
+    {
+      _title = newTitle;
+      _albumList.Add(this);
+      _id = _albumList.Count;
+    }
+    public string GetArtist()
+    {
+      return _artist;
+    }
+
+    public void SetArtist(string newArtist)
+    {
+      _artist = newArtist;
+    }
+
+    public void Save()
+    {
+      _albumList.Add(this);
+    }
+  }
+}
